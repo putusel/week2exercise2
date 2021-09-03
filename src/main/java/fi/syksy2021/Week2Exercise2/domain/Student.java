10ecf098a5319a5623b561070637e7a872316d9c
Elina Putus <elina.putus@digitalworkforce.com>
2021-09-03 16:19:04 +0300

    ensimmäinen versio

---
+++ src/main/java/fi/syksy2021/Week2Exercise2/domain/Student.java
@@ -0,0 +1,26 @@
+package fi.syksy2021.Week2Exercise2.domain;
+
+public class Student {
+
+	private String student;
+
+	
+	public Student(String student) {
+		super();
+		this.student = student;
+	}
+
+	public String getStudent() {
+		return student;
+	}
+
+	public void setStudent(String student) {
+		this.student = student;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [student=" + student + "]";
+	}
+	
+}
---
+++ src/main/java/fi/syksy2021/Week2Exercise2/web/StudentListController.java
@@ -0,0 +1,30 @@
+package fi.syksy2021.Week2Exercise2.web;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.syksy2021.Week2Exercise2.domain.Student;
+
+@Controller
+public class StudentListController {
+	
+			
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String showStudents(Model model) {
+		
+		ArrayList<Student> students = new ArrayList<>();
+		students.add(new Student("John Smith"));
+		students.add(new Student("Anna Jackson"));
+		students.add(new Student("Mary Scott"));
+		
+		model.addAttribute("Students", students);
+		return "studentlist";
+	
+	}
+
+}
