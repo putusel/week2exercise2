2e81a05ca355f509e4571f88f44b18daa5771b28
putusel <89791128+putusel@users.noreply.github.com>
2021-09-05 16:21:40 +0300

    Add files via upload

---
+++ Week2Exercise2/src/main/java/fi/syksy2021/Week2Exercise2/Week2Exercise2Application.java
@@ -0,0 +1,13 @@
+package fi.syksy2021.Week2Exercise2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2Exercise2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2Exercise2Application.class, args);
+	}
+
+}
